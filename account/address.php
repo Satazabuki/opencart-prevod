b5e09b5f6e5a288b69455dc8659aafbde4a4838c
Satazabuki <budjicaaa@gmail.com>
2016-09-19 00:49:23 +0200

    Add files via upload

---
+++ account/address.php
@@ -0,0 +1,37 @@
+﻿<?php
+// Heading
+$_['heading_title']        = 'Adresa';
+
+// Text
+$_['text_account']         = 'Nalog';
+$_['text_address_book']    = 'Unete Adrese';
+$_['text_edit_address']    = 'Izmena Adrese';
+$_['text_add']             = 'Vaša adresa je uspešno dodata';
+$_['text_edit']            = 'Vaša adresa je uspešno ažurirana';
+$_['text_delete']          = 'Vaša adresa je uspešno obrisana';
+$_['text_empty']           = 'Nemate adresa na vašem nalogu.';
+
+// Entry
+$_['entry_firstname']      = 'Ime';
+$_['entry_lastname']       = 'Prezime';
+$_['entry_company']        = 'Firma';
+$_['entry_address_1']      = 'Adresa 1';
+$_['entry_address_2']      = 'Adresa 2';
+$_['entry_postcode']       = 'Poštanski broj';
+$_['entry_city']           = 'Grad';
+$_['entry_country']        = 'Država';
+$_['entry_zone']           = 'Okrug';
+$_['entry_default']        = 'Glavna adresa';
+
+// Error
+$_['error_delete']                = 'Upozorenje: Morate imati barem jednu adresu!';
+$_['error_default']               = 'Upozorenje: Ne možete obrisati glavnu adresu!';
+$_['error_firstname']             = 'Ime mora sadržati između 1 i 32 karaktera!';
+$_['error_lastname']              = 'Prezime mora sadržati između 1 i 32 karaktera!';
+$_['error_vat']                   = 'VAT number is invalid!';
+$_['error_address_1']             = 'Adresa mora sadržati između 3 i 128 karaktera!';
+$_['error_postcode']              = 'Postanski broj mora sadržati između 3 i 128 karaktera!';
+$_['error_city']                  = 'Grad mora sadržati između 2 i 128 karaktera!';
+$_['error_country']               = 'Izaberite državu!';
+$_['error_zone']                  = 'Izaberite okrug!';
+$_['error_custom_field']          = '%s je obavezno!';
