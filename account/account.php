06651939ecce85148932f808615f286bb7df9adb
Satazabuki <budjicaaa@gmail.com>
2016-09-19 00:48:32 +0200

    Create account.php

---
+++ account/account.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']      = 'Moj Nalog';
+
+// Text
+$_['text_account']       = 'Nalog';
+$_['text_my_account']    = 'Moj Nalog';
+$_['text_my_orders']     = 'Moje porudžbine';
+$_['text_my_newsletter'] = 'Obaveštenja';
+$_['text_edit']          = 'Izmena informacija naloga';
+$_['text_password']      = 'Promena šifre';
+$_['text_address']       = 'Promena adrese';
+$_['text_credit_card']   = 'Promena kreditne kartice';
+$_['text_wishlist']      = 'Izmena liste želja';
+$_['text_order']         = 'Istorija porudžbina';
+$_['text_download']      = 'Downloads';
+$_['text_reward']        = 'Vaši poeni';
+$_['text_return']        = 'Pregled zahteva za vraćanje';
+$_['text_transaction']   = 'Vaše Transakcije';
+$_['text_newsletter']    = 'Prijava / Odjava za Obaveštenja';
+$_['text_recurring']     = 'Redovne uplate';
+$_['text_transactions']  = 'Transakcije';
