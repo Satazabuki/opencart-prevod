40654c2b2acc0c91a73eb253341f5259f0e6b099
Satazabuki <budjicaaa@gmail.com>
2016-09-19 11:43:50 +0200

    Update return.php

--- account/return.php
+++ account/return.php
@@ -1,63 +1,63 @@
 <?php
 // Heading
-$_['heading_title']      = 'Product Returns';
+$_['heading_title']      = 'Povraćaj proizvoda';
 
 // Text
-$_['text_account']       = 'Account';
-$_['text_return']        = 'Return Information';
-$_['text_return_detail'] = 'Return Details';
-$_['text_description']   = 'Please complete the form below to request an RMA number.';
-$_['text_order']         = 'Order Information';
-$_['text_product']       = 'Product Information';
-$_['text_reason']        = 'Reason for Return';
-$_['text_message']       = '<p>Thank you for submitting your return request. Your request has been sent to the relevant department for processing.</p><p> You will be notified via e-mail as to the status of your request.</p>';
-$_['text_return_id']     = 'Return ID:';
-$_['text_order_id']      = 'Order ID:';
-$_['text_date_ordered']  = 'Order Date:';
+$_['text_account']       = 'Nalog';
+$_['text_return']        = 'Informacije o povraćaju';
+$_['text_return_detail'] = 'Detalji povraćaja';
+$_['text_description']   = 'Molimo da popunite formu.';
+$_['text_order']         = 'Informacije o porudžbini';
+$_['text_product']       = 'Informacije o proizvodu';
+$_['text_reason']        = 'Razlog za povraćaj';
+$_['text_message']       = '<p>Vaš zahtev za povraćaj je poslat.</p><p> Bićete obavešteni o status putem emaila.</p>';
+$_['text_return_id']     = 'ID povratnice:';
+$_['text_order_id']      = 'ID porudžbine:';
+$_['text_date_ordered']  = 'Datum porudžbine:';
 $_['text_status']        = 'Status:';
-$_['text_date_added']    = 'Date Added:';
-$_['text_comment']       = 'Return Comments';
-$_['text_history']       = 'Return History';
-$_['text_empty']         = 'You have not made any previous returns!';
-$_['text_agree']         = 'I have read and agree to the <a href="%s" class="agree"><b>%s</b></a>';
+$_['text_date_added']    = 'Dodato:';
+$_['text_comment']       = 'Komentar povraćaja';
+$_['text_history']       = 'Istorija povraćaja';
+$_['text_empty']         = 'Nemate predhodnih povraćaja!';
+$_['text_agree']         = 'Pročitao sam i slažem se sa <a href="%s" class="agree"><b>%s</b></a>';
 
 // Column
-$_['column_return_id']   = 'Return ID';
-$_['column_order_id']    = 'Order ID';
+$_['column_return_id']   = 'ID povratnice';
+$_['column_order_id']    = 'ID porudžbine';
 $_['column_status']      = 'Status';
-$_['column_date_added']  = 'Date Added';
-$_['column_customer']    = 'Customer';
-$_['column_product']     = 'Product Name';
+$_['column_date_added']  = 'Dodato';
+$_['column_customer']    = 'Kupac';
+$_['column_product']     = 'Ime proizvoda';
 $_['column_model']       = 'Model';
-$_['column_quantity']    = 'Quantity';
-$_['column_price']       = 'Price';
-$_['column_opened']      = 'Opened';
-$_['column_comment']     = 'Comment';
-$_['column_reason']      = 'Reason';
-$_['column_action']      = 'Action';
+$_['column_quantity']    = 'Količina';
+$_['column_price']       = 'Cena';
+$_['column_opened']      = 'Otvoren';
+$_['column_comment']     = 'Komentar';
+$_['column_reason']      = 'Razlog';
+$_['column_action']      = 'Akcija';
 
 // Entry
-$_['entry_order_id']     = 'Order ID';
-$_['entry_date_ordered'] = 'Order Date';
-$_['entry_firstname']    = 'First Name';
-$_['entry_lastname']     = 'Last Name';
+$_['entry_order_id']     = 'ID porudžbine';
+$_['entry_date_ordered'] = 'Datum porudžbine';
+$_['entry_firstname']    = 'Ime';
+$_['entry_lastname']     = 'Prezime';
 $_['entry_email']        = 'E-Mail';
-$_['entry_telephone']    = 'Telephone';
-$_['entry_product']      = 'Product Name';
-$_['entry_model']        = 'Product Code';
-$_['entry_quantity']     = 'Quantity';
-$_['entry_reason']       = 'Reason for Return';
-$_['entry_opened']       = 'Product is opened';
-$_['entry_fault_detail'] = 'Faulty or other details';
+$_['entry_telephone']    = 'Telefon';
+$_['entry_product']      = 'Ime proizvoda';
+$_['entry_model']        = 'Šifra proizvoda';
+$_['entry_quantity']     = 'Količina';
+$_['entry_reason']       = 'Razlog za vraćanje';
+$_['entry_opened']       = 'Proizvod je otvoren';
+$_['entry_fault_detail'] = 'Neispravan ili drugi detalji';
 
 // Error
-$_['text_error']         = 'The returns you requested could not be found!';
-$_['error_order_id']     = 'Order ID required!';
-$_['error_firstname']    = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']     = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']        = 'E-Mail Address does not appear to be valid!';
-$_['error_telephone']    = 'Telephone must be between 3 and 32 characters!';
-$_['error_product']      = 'Product Name must be greater than 3 and less than 255 characters!';
-$_['error_model']        = 'Product Model must be greater than 3 and less than 64 characters!';
-$_['error_reason']       = 'You must select a return product reason!';
-$_['error_agree']        = 'Warning: You must agree to the %s!';
+$_['text_error']         = 'Traženi povraćaj nije pronađen!';
+$_['error_order_id']     = 'ID porudžbine je obavezan!';
+$_['error_firstname']    = 'Ime mora da sadrži između 1 i 32 karaktera!';
+$_['error_lastname']     = 'Prezime mora da sadrži između 1 i 32 karaktera!';
+$_['error_email']        = 'E-Mail Adresa nije validna!';
+$_['error_telephone']    = 'Telefon mora da sadrži između 3 i 32 karaktera!';
+$_['error_product']      = 'Ime proizvoda mora da sadrži između 3 i 255 karaktera!';
+$_['error_model']        = 'Model proizvoda mora da sadrži izmeđ 3 i 64 karaktera!';
+$_['error_reason']       = 'Morate izabrati razlog za povraćaj!';
+$_['error_agree']        = 'Upozorenje: Morate da se složite sa %s!';
