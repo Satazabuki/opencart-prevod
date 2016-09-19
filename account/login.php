ef591d785db2814d66f6df6a95a305e81ae5e8ab
Shagrath78 <veljomasic@yahoo.com>
2016-09-19 11:50:02 +0200

    Update login.php

--- account/login.php
+++ account/login.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']                = 'Account Login';
+$_['heading_title']                = 'Pristup Nalogu';
 
 // Text
-$_['text_account']                 = 'Account';
-$_['text_login']                   = 'Login';
-$_['text_new_customer']            = 'New Customer';
-$_['text_register']                = 'Register Account';
-$_['text_register_account']        = 'By creating an account you will be able to shop faster, be up to date on an order\'s status, and keep track of the orders you have previously made.';
-$_['text_returning_customer']      = 'Returning Customer';
-$_['text_i_am_returning_customer'] = 'I am a returning customer';
-$_['text_forgotten']               = 'Forgotten Password';
+$_['text_account']                 = 'Nalog';
+$_['text_login']                   = 'Pristup';
+$_['text_new_customer']            = 'Novi kupac';
+$_['text_register']                = 'Registracija Naloga';
+$_['text_register_account']        = 'Otvaranjem naloga bićete u mogućnosti da kupujete brže.Budite u toku pratite porudzbinu\'s status, budite u toku i sa ranijim porudzbinama.';
+$_['text_returning_customer']      = 'Postojeći Kupac';
+$_['text_i_am_returning_customer'] = 'Ja sam postojeći kupac';
+$_['text_forgotten']               = 'Zaboravljena lozinka';
 
 // Entry
-$_['entry_email']                  = 'E-Mail Address';
-$_['entry_password']               = 'Password';
+$_['entry_email']                  = 'E-Mail Adresa';
+$_['entry_password']               = 'Lozinka';
 
 // Error
-$_['error_login']                  = 'Warning: No match for E-Mail Address and/or Password.';
-$_['error_attempts']               = 'Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour.';
-$_['error_approved']               = 'Warning: Your account requires approval before you can login.';
+$_['error_login']                  = 'Upozorenje: Ne slažu se e-mail ili lozinka.';
+$_['error_attempts']               = 'Upozorenje: Previše neuspelih pokušaja prijave.Molimo vas pokušajte ponovo za 1 sat.';
+$_['error_approved']               = 'Upozorenje: Vašem nalogu je potrebno odobrenje pre prijavljivanja.';
