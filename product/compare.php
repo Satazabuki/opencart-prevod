0481a8c548b9de4d0f7a925b1d968263f106b40a
Satazabuki <budjicaaa@gmail.com>
2016-09-19 14:23:09 +0200

    Update compare.php

--- product/compare.php
+++ product/compare.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']     = 'Product Comparison';
+$_['heading_title']     = 'Poređenje proizvoda';
 
 // Text
-$_['text_product']      = 'Product Details';
-$_['text_name']         = 'Product';
-$_['text_image']        = 'Image';
-$_['text_price']        = 'Price';
+$_['text_product']      = 'Detalji proizvoda';
+$_['text_name']         = 'Proizvod';
+$_['text_image']        = 'Slika';
+$_['text_price']        = 'Cena';
 $_['text_model']        = 'Model';
-$_['text_manufacturer'] = 'Brand';
-$_['text_availability'] = 'Availability';
-$_['text_instock']      = 'In Stock';
-$_['text_rating']       = 'Rating';
-$_['text_reviews']      = 'Based on %s reviews.';
-$_['text_summary']      = 'Summary';
-$_['text_weight']       = 'Weight';
-$_['text_dimension']    = 'Dimensions (L x W x H)';
-$_['text_compare']      = 'Product Compare (%s)';
-$_['text_success']      = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">product comparison</a>!';
-$_['text_remove']       = 'Success: You have modified your product comparison!';
-$_['text_empty']        = 'You have not chosen any products to compare.';
+$_['text_manufacturer'] = 'Marka';
+$_['text_availability'] = 'Dostupnost';
+$_['text_instock']      = 'Na stanju';
+$_['text_rating']       = 'Rejting';
+$_['text_reviews']      = 'Obračunato na %s recenzija.';
+$_['text_summary']      = 'Rezime';
+$_['text_weight']       = 'Težina';
+$_['text_dimension']    = 'Deimenzije (L x W x H)';
+$_['text_compare']      = 'Poređenje proizvoda (%s)';
+$_['text_success']      = 'Uspešno: Dodali ste <a href="%s">%s</a> u vašu <a href="%s">listu poređenja</a>!';
+$_['text_remove']       = 'Uspešno: Uspešno ste izmenili poređenje!';
+$_['text_empty']        = 'Niste izabrali ni jedan proizvod za poređenje.';
