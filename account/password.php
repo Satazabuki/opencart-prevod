2d5a13bae8c5d5ade511c039860bf48a4c5d67d5
Shagrath78 <veljomasic@yahoo.com>
2016-09-19 12:34:07 +0200

    Update password.php

--- account/password.php
+++ account/password.php
@@ -1,16 +1,16 @@
 <?php
 // Heading
-$_['heading_title']  = 'Change Password';
+$_['heading_title']  = 'Promenite Lozinku';
 
 // Text
-$_['text_account']   = 'Account';
-$_['text_password']  = 'Your Password';
-$_['text_success']   = 'Success: Your password has been successfully updated.';
+$_['text_account']   = 'Nalog';
+$_['text_password']  = 'Vaša Lozinka';
+$_['text_success']   = 'Uspešno:Vaša lozinka je uspešno ažurirana.';
 
 // Entry
-$_['entry_password'] = 'Password';
-$_['entry_confirm']  = 'Password Confirm';
+$_['entry_password'] = 'Lozinka';
+$_['entry_confirm']  = 'Potvrdite Lozinku';
 
 // Error
-$_['error_password'] = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']  = 'Password confirmation does not match password!';
+$_['error_password'] = 'Lozinka mora da sadrži izmedju 4 i 20 karaktera!';
+$_['error_confirm']  = 'Potvrda za lozinku se ne poklapa sa vašom lozinkom!';
