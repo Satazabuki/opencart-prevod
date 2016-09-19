f6c5ab47c959844a4fa62e37f931d26684f320fc
Shagrath78 <veljomasic@yahoo.com>
2016-09-19 11:03:29 +0200

    Update edit.php

--- account/edit.php
+++ account/edit.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']      = 'My Account Information';
+$_['heading_title']      = 'Moj Nalog';
 
 // Text
-$_['text_account']       = 'Account';
-$_['text_edit']          = 'Edit Information';
-$_['text_your_details']  = 'Your Personal Details';
-$_['text_success']       = 'Success: Your account has been successfully updated.';
+$_['text_account']       = 'Nalog';
+$_['text_edit']          = 'Prikaži Informacije';
+$_['text_your_details']  = 'Vaši Lični Podatci';
+$_['text_success']       = 'Vaš nalog je uspešno ažuriran.';
 
 // Entry
-$_['entry_firstname']    = 'First Name';
-$_['entry_lastname']     = 'Last Name';
+$_['entry_firstname']    = 'Ime';
+$_['entry_lastname']     = 'Prezime';
 $_['entry_email']        = 'E-Mail';
-$_['entry_telephone']    = 'Telephone';
+$_['entry_telephone']    = 'Telefon';
 $_['entry_fax']          = 'Fax';
 
 // Error
-$_['error_exists']                = 'Warning: E-Mail address is already registered!';
-$_['error_firstname']             = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']              = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']                 = 'E-Mail Address does not appear to be valid!';
-$_['error_telephone']             = 'Telephone must be between 3 and 32 characters!';
-$_['error_custom_field']          = '%s required!';
+$_['error_exists']                = 'Upozorenje: E-Mail adresa je već registrovana!';
+$_['error_firstname']             = 'Ime mora da sadrži između 1 i 32 karaktera!';
+$_['error_lastname']              = 'Prezime mora da sadrži između 1 and 32 karaktera!';
+$_['error_email']                 = 'E-Mail adresa nije validna!';
+$_['error_telephone']             = 'Telefon mora da sadrži između 3 and 32 characters!';
+$_['error_custom_field']          = '%s Neophodno!';
