2df2c90a6810c56aab2663e5942a5a77e5d12cc4
Satazabuki <budjicaaa@gmail.com>
2016-09-19 11:46:46 +0200

    Update reset.php

--- account/reset.php
+++ account/reset.php
@@ -1,17 +1,17 @@
 <?php
 // header
-$_['heading_title']  = 'Reset your password';
+$_['heading_title']  = 'Reset lozinke';
 
 // Text
-$_['text_account']   = 'Account';
-$_['text_password']  = 'Enter the new password you wish to use.';
-$_['text_success']   = 'Success: Your password has been successfully updated.';
+$_['text_account']   = 'Nalog';
+$_['text_password']  = 'Unesite novu lozinku koju koju ćete koristiti.';
+$_['text_success']   = 'Upozorenje: Vaša lozinka je uspešno aćurirana.';
 
 // Entry
-$_['entry_password'] = 'Password';
-$_['entry_confirm']  = 'Confirm';
+$_['entry_password'] = 'Lozinka';
+$_['entry_confirm']  = 'Potvrda lozinke';
 
 // Error
-$_['error_password'] = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']  = 'Password and password confirmation do not match!';
-$_['error_code']     = 'Password reset code is invalid or was used previously!';
+$_['error_password'] = 'Lozinka mora da sadrži između 4 i 20 karaktera!';
+$_['error_confirm']  = 'Lozinka i potvrda lozinke se ne slažu!';
+$_['error_code']     = 'Kod za reset lozinke nije ispravan ili je već iskorišćen!';
