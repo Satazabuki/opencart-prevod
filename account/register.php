75e9afe64bdfc34250be7ed5fdda9955f32a5e8e
Satazabuki <budjicaaa@gmail.com>
2016-09-19 11:52:29 +0200

    Update register.php

--- account/register.php
+++ account/register.php
@@ -1,47 +1,47 @@
 <?php
 // Heading
-$_['heading_title']        = 'Register Account';
+$_['heading_title']        = 'Registracija naloga';
 
 // Text
-$_['text_account']         = 'Account';
-$_['text_register']        = 'Register';
-$_['text_account_already'] = 'If you already have an account with us, please login at the <a href="%s">login page</a>.';
-$_['text_your_details']    = 'Your Personal Details';
-$_['text_your_address']    = 'Your Address';
-$_['text_newsletter']      = 'Newsletter';
-$_['text_your_password']   = 'Your Password';
-$_['text_agree']           = 'I have read and agree to the <a href="%s" class="agree"><b>%s</b></a>';
+$_['text_account']         = 'Nalog';
+$_['text_register']        = 'Registracija';
+$_['text_account_already'] = 'Ako već imate registrovan nalog molimo vas da mu <a href="%s">pristupite ovde</a>.';
+$_['text_your_details']    = 'Vaši lični detalji';
+$_['text_your_address']    = 'Vaša adresa';
+$_['text_newsletter']      = 'Obaveštenja';
+$_['text_your_password']   = 'Vaša lozinka';
+$_['text_agree']           = 'Slažem se sa <a href="%s" class="agree"><b>%s</b></a>';
 
 // Entry
-$_['entry_customer_group'] = 'Customer Group';
-$_['entry_firstname']      = 'First Name';
-$_['entry_lastname']       = 'Last Name';
+$_['entry_customer_group'] = 'Grupa kupaca';
+$_['entry_firstname']      = 'Ime';
+$_['entry_lastname']       = 'Prezime';
 $_['entry_email']          = 'E-Mail';
-$_['entry_telephone']      = 'Telephone';
+$_['entry_telephone']      = 'Telefon';
 $_['entry_fax']            = 'Fax';
-$_['entry_company']        = 'Company';
-$_['entry_address_1']      = 'Address 1';
-$_['entry_address_2']      = 'Address 2';
-$_['entry_postcode']       = 'Post Code';
-$_['entry_city']           = 'City';
-$_['entry_country']        = 'Country';
-$_['entry_zone']           = 'Region / State';
-$_['entry_newsletter']     = 'Subscribe';
-$_['entry_password']       = 'Password';
-$_['entry_confirm']        = 'Password Confirm';
+$_['entry_company']        = 'Kompanija';
+$_['entry_address_1']      = 'Adresa 1';
+$_['entry_address_2']      = 'Adresa 2';
+$_['entry_postcode']       = 'poštanski broj';
+$_['entry_city']           = 'Grad';
+$_['entry_country']        = 'Država';
+$_['entry_zone']           = 'Regija';
+$_['entry_newsletter']     = 'Pretplati se';
+$_['entry_password']       = 'Lozinka';
+$_['entry_confirm']        = 'Potvrda lozinke';
 
 // Error
-$_['error_exists']         = 'Warning: E-Mail Address is already registered!';
-$_['error_firstname']      = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']       = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']          = 'E-Mail Address does not appear to be valid!';
-$_['error_telephone']      = 'Telephone must be between 3 and 32 characters!';
-$_['error_address_1']      = 'Address 1 must be between 3 and 128 characters!';
-$_['error_city']           = 'City must be between 2 and 128 characters!';
-$_['error_postcode']       = 'Postcode must be between 2 and 10 characters!';
-$_['error_country']        = 'Please select a country!';
-$_['error_zone']           = 'Please select a region / state!';
-$_['error_custom_field']   = '%s required!';
-$_['error_password']       = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']        = 'Password confirmation does not match password!';
-$_['error_agree']          = 'Warning: You must agree to the %s!';
+$_['error_exists']         = 'Upozorenje: E-Mail adresa je već registrovana!';
+$_['error_firstname']      = 'Ime mora da sadrži između 1 i 32 karaktera!';
+$_['error_lastname']       = 'Prezime mora da sadrži između 1 i 32 karaktera!';
+$_['error_email']          = 'E-Mail Adresa nije ispravna!';
+$_['error_telephone']      = 'Telefon mora da sadrži između 3 i 32 karaktera!';
+$_['error_address_1']      = 'Address 1 mora da sadrži između 3 i 128 karaktera!';
+$_['error_city']           = 'Grad mora da sadrži između 2 i 128 karaktera!';
+$_['error_postcode']       = 'Postanski broj mora da sadrži između 2 i 10 karaktera!';
+$_['error_country']        = 'Molimo izaberite državu!';
+$_['error_zone']           = 'Molimo izaberite regiju';
+$_['error_custom_field']   = '%s je obavezno!';
+$_['error_password']       = 'Lozinka mora da sadrži između 4 i 20 karaktera!';
+$_['error_confirm']        = 'Potvrda lozinke se ne podudara sa lozinkom';
+$_['error_agree']          = 'Upozorenje: Morate se složiti sa %s!';
