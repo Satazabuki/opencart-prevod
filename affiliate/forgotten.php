b5e71daa07f3cf695938a7991dd8ea546326e65c
Shagrath78 <veljomasic@yahoo.com>
2016-09-19 13:57:07 +0200

    Update forgotten.php

--- affiliate/forgotten.php
+++ affiliate/forgotten.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']   = 'Forgot Your Password?';
+$_['heading_title']   = 'Zaboravili ste vašu lozinku?';
 
 // Text
-$_['text_account']    = 'Account';
-$_['text_forgotten']  = 'Forgotten Password';
-$_['text_your_email'] = 'Your E-Mail Address';
-$_['text_email']      = 'Enter the e-mail address associated with your account. Click submit to have your password e-mailed to you';
-$_['text_success']    = 'Success: A new password has been sent to your e-mail address.';
+$_['text_account']    = 'Nalog';
+$_['text_forgotten']  = 'Zaboravljena Lozinka';
+$_['text_your_email'] = 'Vaša E-Mail Adresa';
+$_['text_email']      = 'Unesite e-mail adresu vezanu za vaš račun. Kliknite pošalji i lozinka ce biti poslata na vaš e-mail';
+$_['text_success']    = 'Uspešno: Nova lozinka je poslata na vašu e-mail adresu.';
 
 // Entry
-$_['entry_email']     = 'E-Mail Address';
+$_['entry_email']     = 'E-Mail Adresa';
 
 // Error
-$_['error_email']     = 'Warning: The E-Mail Address was not found in our records, please try again!';
-$_['error_approved']  = 'Warning: Your account requires approval before you can login.';
+$_['error_email']     = 'Upozorenje: E-mail nije pronađen u našoj datoteci,pokušajte ponovo!';
+$_['error_approved']  = 'Upozorenje: Vaš nalog zahteva odobrenje pre prijave.';
