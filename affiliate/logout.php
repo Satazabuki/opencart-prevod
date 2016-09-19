ff91e720007cb96d9c4deaa6ccf7ef1fe1bf1b33
Shagrath78 <veljomasic@yahoo.com>
2016-09-19 14:41:17 +0200

    Update logout.php

--- affiliate/logout.php
+++ affiliate/logout.php
@@ -1,8 +1,8 @@
 <?php
 // Heading
-$_['heading_title'] = 'Account Logout';
+$_['heading_title'] = 'Odjavljivanje sa Naloga';
 
 // Text
-$_['text_message']  = '<p>You have been logged off your Affiliate Account.</p>';
-$_['text_account']  = 'Account';
-$_['text_logout']   = 'Logout';
+$_['text_message']  = '<p>Uspešno ste se odjavili sa vašeg partnerskog naloga.</p>';
+$_['text_account']  = 'Nalog';
+$_['text_logout']   = 'Odjava';
