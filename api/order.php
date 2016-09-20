af6791be35a242dd67980924730d9347316aaea2
Shagrath78 <veljomasic@yahoo.com>
2016-09-20 12:41:43 +0200

    Update order.php

--- api/order.php
+++ api/order.php
@@ -1,16 +1,16 @@
 <?php
 // Text
-$_['text_success']           = 'Success: You have modified orders!';
+$_['text_success']           = 'Uspešno: Prepravili ste narudzbinu!';
 
 // Error
-$_['error_permission']       = 'Warning: You do not have permission to access the API!';
-$_['error_customer']         = 'Warning: Customer details needs to be set!';
-$_['error_payment_address']  = 'Warning: Payment address required!';
-$_['error_payment_method']   = 'Warning: Payment method required!';
-$_['error_no_payment']       = 'Warning: No Payment options are available!';
-$_['error_shipping_address'] = 'Warning: Shipping address required!';
-$_['error_shipping_method']  = 'Warning: Shipping method required!';
-$_['error_no_shipping']      = 'Warning: No Shipping options are available!';
-$_['error_stock']            = 'Warning: Products marked with *** are not available in the desired quantity or not in stock!';
-$_['error_minimum']          = 'Warning: Minimum order amount for %s is %s!';
-$_['error_not_found']        = 'Warning: Order could not be found!';
+$_['error_permission']       = 'Upozorenje: Nemate pristup API!';
+$_['error_customer']         = 'Upozorenje: Potrebno podesiti detalje o kupcu!';
+$_['error_payment_address']  = 'Upozorenje: Potrebna adresa plaćanja!';
+$_['error_payment_method']   = 'Upozorenje: Potreban način plaćanja!';
+$_['error_no_payment']       = 'Upozorenje: Nema dostupne opcije plaćanja!';
+$_['error_shipping_address'] = 'Upozorenje: Potrebna adresa za isporuku!';
+$_['error_shipping_method']  = 'Upozorenje: Potrebna metoda plaćanja!';
+$_['error_no_shipping']      = 'Upozorenje: Opcije za dostavu nisu na raspolaganju!';
+$_['error_stock']            = 'Upozorenje: Proizvodi označeni sa *** nisu dostupni u željenoj količini ili ih nema na stanju!';
+$_['error_minimum']          = 'Upozorenje: Minimalna količina za porudzbinu %s je %s!';
+$_['error_not_found']        = 'Upozorenje: Porudzbinu nemoguće pronaći!';
