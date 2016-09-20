c8b0cd2e55407fdf012302249bfd851f29200ce5
Shagrath78 <veljomasic@yahoo.com>
2016-09-20 12:14:26 +0200

    Update currency.php

--- api/currency.php
+++ api/currency.php
@@ -1,7 +1,7 @@
 <?php
 // Text
-$_['text_success']     = 'Success: Your currency has been changed!';
+$_['text_success']     = 'Uspešno: Vaša valuta je promenjena!';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to access the API!';
-$_['error_currency']   = 'Warning: Currency code is invalid!';
+$_['error_permission'] = 'Upozorenje: Nemate pristup API!';
+$_['error_currency']   = 'Upozorenje: Kod nije validan!';
