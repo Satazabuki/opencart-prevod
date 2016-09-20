df625c6ab82049d2bcc3a021c34d4d66fe8900b4
Shagrath78 <veljomasic@yahoo.com>
2016-09-20 10:25:04 +0200

    Update success.php

--- affiliate/success.php
+++ affiliate/success.php
@@ -6,4 +6,4 @@ $_['heading_title'] = 'Vaš Partnerski nalog je kreiran!';
 $_['text_message']  = '<p>Čestitamo! Vaš novi račun je uspešno kreiran!</p> <p>Sada ste član %s partnera.</p> <p>Ako imate bilo kakva pitanja vezana za uvaj partnerski sistem, molimo vas pošaljite e-mail vlasniku prodavnice.</p> <p>A potvrda je uspešno poslata na e-mail adresu. Ako ne primite potvrdu u roku od sat vremena molimo vas <a href="%s">kontaktirajte nas</a>.</p>';
 $_['text_approval'] = '<p>Hvala vam što ste registrovali partnerski nalog sa %s!</p><p>Bićete obavešteni kad vaš nalog bude aktiviran od strane vlasnika prodavnice.</p><p>,Ako imate bilo kakva pitanja vezana za uvaj partnerski sistem, molimo vas <a href="%s">kontaktirajte vlasnika prodavnice</a>.</p>';
 $_['text_account']  = 'Nalog';
-$_['text_success']  = 'Uspešan';
+$_['text_success']  = 'Uspešno';
