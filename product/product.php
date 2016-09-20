97e87274463dd747ed0ae829dd54c6a714207a7f
Satazabuki <budjicaaa@gmail.com>
2016-09-20 13:42:03 +0200

    Update product.php

--- product/product.php
+++ product/product.php
@@ -1,50 +1,50 @@
 <?php
 // Text
-$_['text_search']              = 'Search';
-$_['text_brand']               = 'Brand';
-$_['text_manufacturer']        = 'Brand:';
-$_['text_model']               = 'Product Code:';
-$_['text_reward']              = 'Reward Points:';
-$_['text_points']              = 'Price in reward points:';
-$_['text_stock']               = 'Availability:';
-$_['text_instock']             = 'In Stock';
-$_['text_tax']                 = 'Ex Tax:';
-$_['text_discount']            = ' or more ';
-$_['text_option']              = 'Available Options';
-$_['text_minimum']             = 'This product has a minimum quantity of %s';
-$_['text_reviews']             = '%s reviews';
-$_['text_write']               = 'Write a review';
-$_['text_login']               = 'Please <a href="%s">login</a> or <a href="%s">register</a> to review';
-$_['text_no_reviews']          = 'There are no reviews for this product.';
-$_['text_note']                = '<span class="text-danger">Note:</span> HTML is not translated!';
-$_['text_success']             = 'Thank you for your review. It has been submitted to the webmaster for approval.';
-$_['text_related']             = 'Related Products';
-$_['text_tags']                = 'Tags:';
-$_['text_error']               = 'Product not found!';
-$_['text_payment_recurring']   = 'Payment Profile';
+$_['text_search']              = 'Pretraga';
+$_['text_brand']               = 'Marka';
+$_['text_manufacturer']        = 'Proizvođač:';
+$_['text_model']               = 'Šifra proizvoda:';
+$_['text_reward']              = 'Nagradni poeni:';
+$_['text_points']              = 'Cena u nagradnim poenima:';
+$_['text_stock']               = 'Dostupnost:';
+$_['text_instock']             = 'Na stanju';
+$_['text_tax']                 = 'Bez takse:';
+$_['text_discount']            = ' ili vise ';
+$_['text_option']              = 'Opcije dostupnosti';
+$_['text_minimum']             = 'Ovaj proizvod ima minimalnu količinu od %s';
+$_['text_reviews']             = '%s recenzija';
+$_['text_write']               = 'Napiši recenziju';
+$_['text_login']               = 'Molimo da se <a href="%s">prijavite</a> ili <a href="%s">registrujete</a> da bi pisali recenziju';
+$_['text_no_reviews']          = 'TNema recenzija za ovaj proizvod.';
+$_['text_note']                = '<span class="text-danger">Upozorenje:</span> HTML kod nije podržan!';
+$_['text_success']             = 'Hvala na vašoj recenziji. Recenzija će biti objavljena posle odobrenja administratora.';
+$_['text_related']             = 'Slični proizvodi';
+$_['text_tags']                = 'Oznake:';
+$_['text_error']               = 'Proizvod nije pronađen!';
+$_['text_payment_recurring']   = 'Profil plaćanja';
 $_['text_trial_description']   = '%s every %d %s(s) for %d payment(s) then';
 $_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
 $_['text_payment_cancel']      = '%s every %d %s(s) until canceled';
-$_['text_day']                 = 'day';
-$_['text_week']                = 'week';
-$_['text_semi_month']          = 'half-month';
-$_['text_month']               = 'month';
-$_['text_year']                = 'year';
+$_['text_day']                 = 'dan';
+$_['text_week']                = 'nedelja';
+$_['text_semi_month']          = 'pola meseca';
+$_['text_month']               = 'mesec';
+$_['text_year']                = 'godina';
 
 // Entry
-$_['entry_qty']                = 'Qty';
-$_['entry_name']               = 'Your Name';
-$_['entry_review']             = 'Your Review';
-$_['entry_rating']             = 'Rating';
-$_['entry_good']               = 'Good';
-$_['entry_bad']                = 'Bad';
+$_['entry_qty']                = 'Kol';
+$_['entry_name']               = 'Vaše ime';
+$_['entry_review']             = 'Vaša recenzija';
+$_['entry_rating']             = 'Rejting';
+$_['entry_good']               = 'Dobro';
+$_['entry_bad']                = 'Loše';
 
 // Tabs
-$_['tab_description']          = 'Description';
-$_['tab_attribute']            = 'Specification';
-$_['tab_review']               = 'Reviews (%s)';
+$_['tab_description']          = 'Opis';
+$_['tab_attribute']            = 'Specifikacija';
+$_['tab_review']               = 'Recenzija (%s)';
 
 // Error
-$_['error_name']               = 'Warning: Review Name must be between 3 and 25 characters!';
-$_['error_text']               = 'Warning: Review Text must be between 25 and 1000 characters!';
-$_['error_rating']             = 'Warning: Please select a review rating!';
+$_['error_name']               = 'Upozorenje: Ime recenzije must mora da sadrži između 3 i 25 karaktera!';
+$_['error_text']               = 'Upozorenje: Tekst recenzije mora da sadrži između 25 i 1000 karaktera!';
+$_['error_rating']             = 'Upozorenje: Molimo izaberite rejting!';
