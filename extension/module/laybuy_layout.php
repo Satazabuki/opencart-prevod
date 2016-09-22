f17eb77bca06356d136627e65f0294a8c52acdb8
Shagrath78 <veljomasic@yahoo.com>
2016-09-22 13:53:45 +0200

    Update laybuy_layout.php

--- extension/module/laybuy_layout.php
+++ extension/module/laybuy_layout.php
@@ -1,26 +1,26 @@
 <?php
 // Heading
-$_['heading_title']            = 'Lay-Buy Information';
+$_['heading_title']            = 'Lay-Buy Informacije';
 
 // Text
-$_['text_reference_info']      = 'Reference Information';
-$_['text_laybuy_ref_no']       = 'Lay-Buy Reference ID:';
-$_['text_paypal_profile_id']   = 'PayPal Profile ID:';
-$_['text_payment_plan']        = 'Payment Plan';
+$_['text_reference_info']      = 'Informacije o Preporuci';
+$_['text_laybuy_ref_no']       = 'Lay-Buy ID Preporuke:';
+$_['text_paypal_profile_id']   = 'PayPal ID Profil:';
+$_['text_payment_plan']        = 'Plan Plaćanja';
 $_['text_status']              = 'Status:';
-$_['text_amount']              = 'Amount:';
-$_['text_downpayment_percent'] = 'Down Payment Percent:';
-$_['text_months']              = 'Months:';
-$_['text_downpayment_amount']  = 'Down Payment Amount:';
-$_['text_payment_amounts']     = 'Payment Amounts:';
-$_['text_first_payment_due']   = 'First Payment Due:';
-$_['text_last_payment_due']    = 'Last Payment Due:';
-$_['text_downpayment']         = 'Down Payment';
-$_['text_month']               = 'Month';
+$_['text_amount']              = 'Iznos:';
+$_['text_downpayment_percent'] = 'Procenat Učešća:';
+$_['text_months']              = 'Meseci:';
+$_['text_downpayment_amount']  = 'Iznos Učesća:';
+$_['text_payment_amounts']     = 'Plaćeni Iznos:';
+$_['text_first_payment_due']   = 'Prvi plaćeni dug:';
+$_['text_last_payment_due']    = 'Poslednji plaćeni dug:';
+$_['text_downpayment']         = 'Kaucija';
+$_['text_month']               = 'Mesec';
 
 // Column
-$_['column_instalment']        = 'Instalment';
-$_['column_amount']            = 'Amount';
-$_['column_date']              = 'Date';
-$_['column_pp_trans_id']       = 'PayPal Transaction ID';
+$_['column_instalment']        = 'Rata';
+$_['column_amount']            = 'Iznos';
+$_['column_date']              = 'Datum';
+$_['column_pp_trans_id']       = 'PayPal ID Transakcije';
 $_['column_status']            = 'Status';
