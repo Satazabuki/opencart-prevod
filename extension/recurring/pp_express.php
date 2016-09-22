e8315934ddf50706ebe92d8855b7737bddc1f0e7
Shagrath78 <veljomasic@yahoo.com>
2016-09-22 14:01:43 +0200

    Update pp_express.php

--- extension/recurring/pp_express.php
+++ extension/recurring/pp_express.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_title']          = 'PayPal Express Checkout';
-$_['text_canceled']       = 'Success: You have succesfully caneled this payment!';
+$_['text_title']          = 'PayPal Brzo odjavljivanje';
+$_['text_canceled']       = 'Uspešno: Uspešno ste otkazali uplatu!';
 
 // Button
-$_['button_cancel']       = 'Cancel Recurring Payment';
+$_['button_cancel']       = 'Otkaži Ponovo Plaćanje';
 
 // Error
-$_['error_not_cancelled'] = 'Error: %s';
-$_['error_not_found']     = 'Could not cancel recurring profile';
+$_['error_not_cancelled'] = 'Greška: %s';
+$_['error_not_found']     = 'Nije moguće otkazati ponovo plaćanje';
