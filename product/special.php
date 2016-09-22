358dd04de84d9ee074fd8e9d60aae03091601ae6
Satazabuki <budjicaaa@gmail.com>
2016-09-22 13:08:26 +0200

    Update special.php

--- product/special.php
+++ product/special.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']     = 'Special Offers';
+$_['heading_title']     = 'Specijalna Ponuda';
 
 // Text
-$_['text_empty']        = 'There are no special offer products to list.';
-$_['text_quantity']     = 'Qty:';
-$_['text_manufacturer'] = 'Brand:';
-$_['text_model']        = 'Product Code:';
-$_['text_points']       = 'Reward Points:';
-$_['text_price']        = 'Price:';
-$_['text_tax']          = 'Ex Tax:';
-$_['text_compare']      = 'Product Compare (%s)';
-$_['text_sort']         = 'Sort By:';
-$_['text_default']      = 'Default';
-$_['text_name_asc']     = 'Name (A - Z)';
-$_['text_name_desc']    = 'Name (Z - A)';
-$_['text_price_asc']    = 'Price (Low &gt; High)';
-$_['text_price_desc']   = 'Price (High &gt; Low)';
-$_['text_rating_asc']   = 'Rating (Lowest)';
-$_['text_rating_desc']  = 'Rating (Highest)';
+$_['text_empty']        = 'Trenutno nema specijalnih ponuda.';
+$_['text_quantity']     = 'Kol:';
+$_['text_manufacturer'] = 'Marka:';
+$_['text_model']        = 'Šifra proizvoda:';
+$_['text_points']       = 'Nagrani poeni:';
+$_['text_price']        = 'Cena:';
+$_['text_tax']          = 'Bez takse:';
+$_['text_compare']      = 'Poređenje proizvoda (%s)';
+$_['text_sort']         = 'Sortiraj po:';
+$_['text_default']      = 'Uobičajeno';
+$_['text_name_asc']     = 'Ime (A - Z)';
+$_['text_name_desc']    = 'Ime (Z - A)';
+$_['text_price_asc']    = 'Cena (Manja &gt; Veća)';
+$_['text_price_desc']   = 'Cena (Veća &gt; Manja)';
+$_['text_rating_asc']   = 'Rejting (Manji)';
+$_['text_rating_desc']  = 'Rejting (Veći)';
 $_['text_model_asc']    = 'Model (A - Z)';
 $_['text_model_desc']   = 'Model (Z - A)';
-$_['text_limit']        = 'Show:';
+$_['text_limit']        = 'Prikaži:';
