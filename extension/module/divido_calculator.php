c6a388e171adc0c543f6a55063541cf75190692b
Shagrath78 <veljomasic@yahoo.com>
2016-09-22 13:35:33 +0200

    Update divido_calculator.php

--- extension/module/divido_calculator.php
+++ extension/module/divido_calculator.php
@@ -1,14 +1,14 @@
 <?php
 // Calculator
-$_['text_checkout_title']      = 'Pay in instalments';
-$_['text_choose_plan']         = 'Choose your plan';
-$_['text_choose_deposit']      = 'Choose your deposit';
-$_['text_monthly_payments']    = 'monthly payments of';
-$_['text_months']              = 'months';
-$_['text_term']                = 'Term';
-$_['text_deposit']             = 'Deposit';
-$_['text_credit_amount']       = 'Cost of credit';
-$_['text_amount_payable']      = 'Total payable';
-$_['text_total_interest']      = 'Total interest APR';
-$_['text_monthly_installment'] = 'Monthly installment';
-$_['text_redirection']         = 'You will be redirected to Divido to complete this finance application when you confirm your order';
+$_['text_checkout_title']      = 'Plaćanje na rate';
+$_['text_choose_plan']         = 'Izaberite vaš plan';
+$_['text_choose_deposit']      = 'Izaberite vaš depozit';
+$_['text_monthly_payments']    = 'mesečne uplate';
+$_['text_months']              = 'meseci';
+$_['text_term']                = 'Uslovi';
+$_['text_deposit']             = 'Depozit';
+$_['text_credit_amount']       = 'Veličina Kredita';
+$_['text_amount_payable']      = 'Totalne Uplate';
+$_['text_total_interest']      = 'Totalna kamata APR';
+$_['text_monthly_installment'] = 'Mesečna Rata';
+$_['text_redirection']         = 'Bićete redirektovani kod Divido da završite ovu finansijksu aplikaciju kad potvrdite porudzbinu';
