e4e65f2c6f9c33b66c9b2a44f6abc869567c461d
Satazabuki <budjicaaa@gmail.com>
2016-09-22 13:23:53 +0200

    Update voucher.php

--- mail/voucher.php
+++ mail/voucher.php
@@ -1,8 +1,8 @@
 <?php
 // Text
-$_['text_subject']  = 'You have been sent a gift certificate from %s';
-$_['text_greeting'] = 'Congratulations, You have received a Gift Certificate worth %s';
-$_['text_from']     = 'This Gift Certificate has been sent to you by %s';
-$_['text_message']  = 'With a message saying';
-$_['text_redeem']   = 'To redeem this Gift Certificate, write down the redemption code which is <b>%s</b> then click on the the link below and purchase the product you wish to use this gift certificate on. You can enter the gift certificate code on the shopping cart page before you click checkout.';
-$_['text_footer']   = 'Please reply to this e-mail if you have any questions.';
+$_['text_subject']  = 'Poslat vam je poklon vaučer od %s';
+$_['text_greeting'] = 'Čestitamo, stigao vam je poklon vaučer u vrednosti od %s';
+$_['text_from']     = 'Ovaj poklon vaučer vam je poslat od %s';
+$_['text_message']  = 'Sa pratećom porukom';
+$_['text_redeem']   = 'Da bi iskoristili poklon vaučer, molimo vas zapišite kod koje <b>%s</b> a potokm kliknite na link kako bi ste iskoristili kako bi ste iskoristili vaučer prilikom kupovine. Vaučer možete iskoristiti u vašoj korpi pre samog plaćanja.';
+$_['text_footer']   = 'Molimo odgovorite na ovaj e-mail ako imate bilo kakvih pitanja.';
