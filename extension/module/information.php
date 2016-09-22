f8703bff6b0e033f544d434ebf4832b0cd7bb1e5
Shagrath78 <veljomasic@yahoo.com>
2016-09-22 13:42:40 +0200

    Update information.php

--- extension/module/information.php
+++ extension/module/information.php
@@ -1,7 +1,7 @@
 <?php
 // Heading
-$_['heading_title'] = 'Information';
+$_['heading_title'] = 'Informacije';
 
 // Text
-$_['text_contact']  = 'Contact Us';
-$_['text_sitemap']  = 'Site Map';
+$_['text_contact']  = 'Kontaktirajte Nas';
+$_['text_sitemap']  = 'Mapa Sajta';
