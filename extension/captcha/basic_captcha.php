48a25087550b55d63cfdc62b30b9392eade53f67
Shagrath78 <veljomasic@yahoo.com>
2016-09-22 13:08:06 +0200

    Update basic_captcha.php

--- extension/captcha/basic_captcha.php
+++ extension/captcha/basic_captcha.php
@@ -1,9 +1,9 @@
 <?php
 // Text
-$_['text_captcha'] = 'Captcha';
+$_['text_captcha'] = 'Siguronosni Kod';
 
 // Entry
-$_['entry_captcha'] = 'Enter the code in the box below';
+$_['entry_captcha'] = 'Unesite kod u polje ispod';
 
 // Error
-$_['error_captcha'] = 'Verification code does not match the image!';
+$_['error_captcha'] = 'Verifikacioni kod se ne poklapa sa slikom !';
