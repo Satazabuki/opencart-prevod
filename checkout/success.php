165e5623d41bcb96cd1ee212ef80969628b7c10a
Shagrath78 <veljomasic@yahoo.com>
2016-09-23 15:18:37 +0200

    Update success.php

--- checkout/success.php
+++ checkout/success.php
@@ -4,7 +4,7 @@ $_['heading_title']        = 'Vaša porudzbina je primljena!';
 
 // Text
 $_['text_basket']          = 'Potrošačka korpa';
-$_['text_checkout']        = 'Odjava';
+$_['text_checkout']        = 'Plaćanje';
 $_['text_success']         = 'Uspešna';
 $_['text_customer']        = '<p>Vaša porudzbina je uspešno obradjena!</p><p>Možete da vidite istoriju poručivanja putem <a href="%s">moj nalog</a> stranice i kliknete na <a href="%s">istoriju</a>.</p><p>Ako vaša kupovina ima vezana preuzimanja, možete otići na nalog <a href="%s">preuzimanja</a> stranicu da bi ih videli.</p><p>molimo vas pitanja prosledite <a href="%s">vlasniku prodavnice</a>.</p><p>Hvala vam sto koristite naše online usluge!</p>';
 $_['text_guest']           = '<p>Vaša porudzbina je uspešno obradjena!</p><p>Molimo vas pitanja prosledite <a href="%s">vlasniku prodavnice</a>.</p><p>Hvala vam sto koristite naše online usluge!</p>';
