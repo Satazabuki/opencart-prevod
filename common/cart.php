862f8f2753739aaa16bda50a2a34106450ce7e60
Satazabuki <budjicaaa@gmail.com>
2016-09-23 14:47:22 +0200

    Update cart.php

--- common/cart.php
+++ common/cart.php
@@ -3,5 +3,5 @@
 $_['text_items']     = '%s Stavka(e) - %s';
 $_['text_empty']     = 'Vaša potrošačka korpa je prazna!';
 $_['text_cart']      = 'Pogledajte Korpu';
-$_['text_checkout']  = 'Odjava';
+$_['text_checkout']  = 'Plaćanje';
 $_['text_recurring'] = 'Profil Plaćanja';
