098545e32ccd044475ad6c3c83a8970d79be8568
Satazabuki <budjicaaa@gmail.com>
2016-09-23 15:13:24 +0200

    Update cheque.php

--- extension/payment/cheque.php
+++ extension/payment/cheque.php
@@ -1,7 +1,7 @@
 <?php
 // Text
-$_['text_title']				= 'Cheque / Money Order';
-$_['text_instruction']			= 'Cheque / Money Order Instructions';
-$_['text_payable']				= 'Make Payable To: ';
-$_['text_address']				= 'Send To: ';
-$_['text_payment']				= 'Your order will not ship until we receive payment.';
+$_['text_title']				= 'Plaćanje pouzećem';
+$_['text_instruction']			= 'Instrukcije';
+$_['text_payable']				= ': ';
+$_['text_address']				= ': ';
+$_['text_payment']				= 'Plaćanje je prilikom preuzimanja pordudžbine na vašoj adresi.';
