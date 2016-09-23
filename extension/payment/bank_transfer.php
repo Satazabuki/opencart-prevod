a620d98e01ebd7c056aeb1c8ec0f8f1b3493ed6b
Satazabuki <budjicaaa@gmail.com>
2016-09-23 15:05:06 +0200

    Update bank_transfer.php

--- extension/payment/bank_transfer.php
+++ extension/payment/bank_transfer.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_title']				= 'Bank Transfer';
-$_['text_instruction']			= 'Bank Transfer Instructions';
-$_['text_description']			= 'Please transfer the total amount to the following bank account.';
-$_['text_payment']				= 'Your order will not ship until we receive payment.';
+$_['text_title']				= 'PostNet Uputnica';
+$_['text_instruction']			= 'PostNet instrukcije plaćanja';
+$_['text_description']			= 'Molimo da celokupnu sumu uplatite na date podatke.';
+$_['text_payment']				= 'Vaša porudžbina neće biti poslata dok se uplata ne proknjiži.';
