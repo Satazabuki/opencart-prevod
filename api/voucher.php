6ae36257fef91268a23a1e1a19732633914565df
Shagrath78 <veljomasic@yahoo.com>
2016-09-23 15:25:33 +0200

    Update voucher.php

--- api/voucher.php
+++ api/voucher.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_success']     = 'Uspešno: vaš poklon vaučer je aktiviran!';
+$_['text_success']     = 'Uspešno: Vaš poklon vaučer je aktiviran!';
 $_['text_cart']        = 'Uspešno: Modifikovali ste vašu potrošačku korpu!';
 $_['text_for']         = '%s Poklon Vaučer za %s';
 
 // Error
-$_['error_permission'] = 'Upozorenje: nemate dozvolu za pristup API!';
+$_['error_permission'] = 'Upozorenje: Nemate dozvolu za pristup API!';
 $_['error_voucher']    = 'Upozorenje: Poklon vaučer nije ispravan ili ste potrošili sav iznos!';
 $_['error_to_name']    = 'Ime Primaoca\ mora da sadrži između 1 i 64 karaktera!';
 $_['error_from_name']  = 'Vaše ime mora da sadrži između  1 i 64 karaktera!';
