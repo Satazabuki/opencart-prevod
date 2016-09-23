894ae81292d4f9ea58ca7df881821239ca07d40a
Satazabuki <budjicaaa@gmail.com>
2016-09-23 15:15:30 +0200

    Update cheque.php

--- extension/payment/cheque.php
+++ extension/payment/cheque.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['text_title']				= 'Plaćanje pouzećem';
-$_['text_instruction']			= 'Instrukcije';
-$_['text_payable']				= ': ';
-$_['text_address']				= ': ';
+$_['text_instruction']			= 'Instrukcije plaćanja';
+$_['text_payable']				= ' ';
+$_['text_address']				= ' ';
 $_['text_payment']				= 'Plaćanje je prilikom preuzimanja pordudžbine na vašoj adresi.';
